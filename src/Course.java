f8fd1495e5e66aacd12db4fc837205fd15d5bd28
Admin <Admin@DESKTOP-RI0URUI>
2023-05-03 14:22:50 +0800

    Tselmeg

---
+++ src/Course.java
@@ -0,0 +1,17 @@
+
+public class Course {
+		// TODO Auto-generated method stub
+		String courseName;
+		
+		Course (String courseName) {
+			this.courseName = courseName;
+		}
+		public String getCourseName() {
+			return courseName;
+		}
+		public void setCourseName(String courseName) {
+			this.courseName = courseName;
+
+	}
+
+}
